5165dd8ee3cc8cfd0c52f490415ef08e98be086b
dideex <wc34fun@mail.ru>
2019-07-23 11:39:29 +0800

    Added macro

--- 01/backend/lib/backend/greeter.ex
+++ 01/backend/lib/backend/greeter.ex
@@ -5,6 +5,18 @@ defmodule Backend.Greeter do
   """
   @greeting Application.fetch_env!(:backend, :greeting)
 
+  defmacro __using__(_) do
+    quote do
+      def good_wish, do: "Have a nice day!"
+    end
+  end
+
+  defmacro concat({:+, _, [lhs, rhs]}) do
+    quote do
+      "#{unquote(lhs)} <> #{unquote(rhs)}"
+    end
+  end
+
   def greeting(name) do
     @greeting <> ", #{name}"
   end
--- 01/backend/lib/tasks/greeting.ex
+++ 01/backend/lib/tasks/greeting.ex
@@ -1,13 +1,19 @@
 defmodule Mix.Tasks.Greeting do
   use Mix.Task
+  use Backend.Greeter
   alias Backend.Greeter, as: Greeter
   alias Mix.Shell.IO
+  # require Backend.Greeter
+  import Backend.Greeter, only: [concat: 1]
 
   def run(args) do
-    args
+    greeting = args
       |> List.first()
       |> (&(&1 || "anonymouse")).()
       |> Greeter.greeting()
+
+    greeting + good_wish()
+      |> concat()
       |> IO.info()
   end
 end
