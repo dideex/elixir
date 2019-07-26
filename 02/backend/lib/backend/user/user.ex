ae3278e5db3d640f2e7e939e9c3761a993f04cc0
dideex <wc34fun@mail.ru>
2019-07-26 20:09:26 +0800

    Added user model

---
+++ 02/backend/lib/backend/user/user.ex
@@ -0,0 +1,32 @@
+defmodule Backend.User do
+  use Ecto.Schema
+  require Ecto.Query
+  alias Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password, :string
+    field :age, :integer
+
+    has_one :avatar, Backend.User.Avatar
+  end
+
+  def changeset(user, params \\ %{}) do
+    user
+      |> Changeset.cast(params, [:email, :name, :password, :age])
+      |> Changeset.validate_required([:name, :password])
+      |> Changeset.validate_length(:name, min: 3)
+  end
+
+  def get_first_record do
+    Backend.Repo.one(Ecto.Query.from u in __MODULE__, order_by: [asc: u.id], limit: 1)
+  end
+
+  def get_last_record do
+    __MODULE__
+      |> Ecto.Query.last()
+      |> Backend.Repo.one()
+  end
+
+end
--- 02/backend/priv/repo/migrations/20190725113221_create_users_table/20190725113221_create_users_table.exs
+++ 02/backend/priv/repo/migrations/20190725113221_create_users_table/20190725113221_create_users_table.exs
@@ -1,16 +1,12 @@
 defmodule :"Elixir.Backend.Repo.Migrations.CreateUsersTable.exs" do
   use Ecto.Migration
 
-  def up do
+  def change do
     create table(:users) do
       add :name, :string
       add :email, :string
       add :password, :string
       add :age, :integer
     end
-  end
-
-  def down do
-    drop table(:users)
   end
 end
