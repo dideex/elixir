3af42a8e122681b23ad517cdddba9c133455825e
dideex <wc34fun@mail.ru>
2019-08-06 11:11:05 +0800

    Added about page

--- 03/backend/lib/backend_web/controllers/page_controller.ex
+++ 03/backend/lib/backend_web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule BackendWeb.PageController do
   use BackendWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  def index(connection, _params) do
+    render(connection, "index.html")
+  end
+
+  def about(connection, _params) do
+    render(connection, "about.html")
   end
 end
