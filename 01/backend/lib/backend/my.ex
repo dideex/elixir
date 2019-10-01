d37d4c8e859852168b02862709b81bfc9d96ad04
dideex <wc34fun@mail.ru>
2019-10-01 19:29:40 +0300

    Added test package

---
+++ 01/backend/lib/backend/my.ex
@@ -0,0 +1,36 @@
+defmodule Backend.My do
+  def adjacentElementsProduct(inputArray) do
+    inputArray
+    |> mulList()
+    |> Enum.max()
+  end
+
+  defp mulList([_]) do
+    []
+  end
+
+  defp mulList([first | rest]) do
+    [second | _] = rest
+    [first * second | mulList(rest)]
+  end
+
+  def almostIncreasingSequence(sequence) do
+    sequence
+    # |> Enum.zip(tl(sequence))
+    |> tl()
+    |> Enum.reduce(sequence, fn x, acc ->
+      if hd(acc) < x do
+        tl(acc)
+      else
+        acc
+      end
+    end)
+    # |> (&(length(&1) <= 2)).()
+
+# 2 5 3 5
+# 1 2 5 3 5
+
+
+    # |> (&(&1 <= 1)).()
+  end
+end
