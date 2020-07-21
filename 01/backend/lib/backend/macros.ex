f4fd392aadf9196b62db4d5707f9fa5d18390869
DiDex <wc34fun@mail.ru>
2020-07-21 09:22:39 +0300

    Added definfo macro

--- 01/backend/lib/backend/macros.ex
+++ 01/backend/lib/backend/macros.ex
@@ -21,6 +21,21 @@ defmodule Macroses do
     end
   end
 
+  defmacro definfo do
+    IO.puts("In macro's context (#{__MODULE__}).")
+
+    quote do
+      IO.puts("In caller's context (#{__MODULE__}).")
+
+      def friendly_info do
+        IO.puts("""
+          My name is #{__MODULE__}
+          """)
+          # My functions are #{inspect(__info__(:functions))}
+      end
+    end
+  end
+
   # iex> require Macroses
   # iex> Macroses.unless 2 == 5, do: "block entered"
   # "block entered"
---
+++ 01/backend/lib/backend/my_module.ex
@@ -0,0 +1,4 @@
+defmodule MyModule do
+  require Macroses
+  Macroses.definfo
+end
